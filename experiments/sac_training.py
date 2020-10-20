9e692e4192ac46bb678a047541eb66c8382bd1ce
Julius Hietala <julius.hietala@gmail.com>
2020-10-20 12:37:38 +0300

    Refactor experimenting

--- experiments/sac_training.py
+++ experiments/sac_training.py
@@ -14,19 +14,42 @@ from rlkit.torch.torch_rl_algorithm import TorchBatchRLAlgorithm
 from rlkit.data_management.obs_dict_replay_buffer import ObsDictRelabelingBuffer
 import gym
 import mujoco_py
+import argparse
 import torch.nn as nn
 import torch
 
+def argsparser():
+    parser = argparse.ArgumentParser("Parser")
+    parser.add_argument('--run',  default=1, type=int)
+    parser.add_argument('--title', default="notitle", type=str)
+    parser.add_argument('--train_steps', default=1000, type=int)
+    parser.add_argument('--num_epochs', default=100, type=int)
+    parser.add_argument('--her_percent', default=0.0, type=float)
+    parser.add_argument('--buffer_size', default=1E6, type=int)
+    parser.add_argument('--max_path_length', default=50, type=int)
+    parser.add_argument('--env_name', type=str, required=True)
+    parser.add_argument('--gpu', default=False, type=bool)
+    parser.add_argument('--image_training', default=False, type=bool)
+    parser.add_argument('--task', type=str, required=True)
+    parser.add_argument('--n_actions', type=int, default=3)
+    parser.add_argument('--learn_grasp', type=str, required=False)
+    return parser.parse_args()
+
 
 def experiment(variant):
-    eval_env = NormalizedBoxEnv(gym.make(variant['env_name']).env)
-    expl_env = NormalizedBoxEnv(gym.make(variant['env_name']).env)
+    eval_env = NormalizedBoxEnv(gym.make(variant['env_name'], **variant['env_kwargs']))
+    expl_env = NormalizedBoxEnv(gym.make(variant['env_name'], **variant['env_kwargs']))
 
-    obs_dim = expl_env.observation_space.spaces['image'].low.size
+    image_training = variant['image_training']
+    if image_training:
+        observation_key = 'image'
+    else:
+        observation_key = 'observation'
+
+    obs_dim = expl_env.observation_space.spaces[observation_key].low.size
     action_dim = eval_env.action_space.low.size
     goal_dim = eval_env.observation_space.spaces['desired_goal'].low.size
 
-    observation_key = 'image'
     desired_goal_key = 'desired_goal'
 
     achieved_goal_key = desired_goal_key.replace("desired", "achieved")
@@ -52,11 +75,20 @@ def experiment(variant):
         output_size=1,
         hidden_sizes=[M, M],
     )
-    policy = MonsterTanhCNNGaussianPolicy(
-        output_size=action_dim,
-        **variant['policy_kwargs'],
-    )
-    
+
+    if image_training:
+        policy = TanhCNNGaussianPolicy(
+            output_size=action_dim,
+            added_fc_input_size=goal_dim,
+            **variant['policy_kwargs'],
+        )
+    else:
+        policy = TanhGaussianPolicy(
+            obs_dim=obs_dim + goal_dim,
+            action_dim=action_dim,
+            hidden_sizes=[M, M],
+            **variant['policy_kwargs']
+        )
     eval_policy = MakeDeterministic(policy)
     eval_path_collector = GoalConditionedPathCollector(
         eval_env,
@@ -101,25 +133,6 @@ def experiment(variant):
     algorithm.to(ptu.device)
     with mujoco_py.ignore_mujoco_warnings():
         algorithm.train()
-        ones = torch.ones(1, 3, 84, 84).view(1,3*84*84)
-        zeros = torch.zeros(1, 3, 84, 84).view(1,3*84*84)
-        policy.make_deterministic()
-        print("zeros", policy(zeros))
-        print("ones", policy(ones))
-        for _ in range(10):
-            rand = torch.rand(1, 3, 84, 84).view(1,3*84*84)
-            outp = policy(rand)
-            print("Inp and outp", rand, outp)
-
-        traced = torch.jit.trace(policy, ones)
-        print("Jit worked")
-        traced.save("traced_policy.pt")
-        print("Saved model")
-        
-    
-
-    
-    
 
 
 
@@ -128,22 +141,15 @@ if __name__ == "__main__":
     # noinspection PyTypeChecker
     variant = dict(
         algorithm="SAC",
-        version="normal",
-        env_name="ClothDiagonalStrictPixels-v1",
         layer_size=256,
         replay_buffer_kwargs=dict(
-            max_size=int(1E4),
-            fraction_goals_rollout_goals=0.2,  #Tune
+            max_size=int(1E6),
             fraction_goals_env_goals=0,
         ),
         algorithm_kwargs=dict(
-            num_epochs=10,
-            num_eval_steps_per_epoch=50,
-            num_train_loops_per_epoch=10,
-            num_trains_per_train_loop=100, 
-            num_expl_steps_per_train_loop=50, #Is this good?
-            min_num_steps_before_training=0,
-            max_path_length=50,
+            num_epochs=100,
+            num_eval_steps_per_epoch=500,
+            min_num_steps_before_training=1000,
             batch_size=256,
         ),
         trainer_kwargs=dict(
@@ -155,7 +161,31 @@ if __name__ == "__main__":
             reward_scale=1,
             use_automatic_entropy_tuning=True,
         ),
-        policy_kwargs=dict(
+    )
+    args = argsparser()
+    file_path = args.title + "-run-" + str(args.run)
+
+    variant['algorithm_kwargs']['num_trains_per_train_loop'] = args.train_steps
+    variant['algorithm_kwargs']['num_expl_steps_per_train_loop'] = args.train_steps
+    variant['algorithm_kwargs']['num_train_loops_per_epoch'] = int(10000 / args.train_steps)
+    variant['algorithm_kwargs']['num_epochs'] = args.num_epochs
+    variant['algorithm_kwargs']['max_path_length'] = int(args.max_path_length)
+    variant['replay_buffer_kwargs']['fraction_goals_rollout_goals'] = 1 - args.her_percent
+    variant['replay_buffer_kwargs']['max_size'] = int(args.buffer_size)
+    variant['env_name'] = args.env_name
+    variant['version'] = args.title
+    variant['image_training'] = args.image_training
+
+    variant['env_kwargs'] = dict(
+        learn_grasp = args.learn_grasp,
+        n_actions=args.n_actions,
+        task=args.task,
+        pixels=args.image_training
+    )
+
+    setup_logger(file_path, variant=variant)
+    if args.image_training:
+        variant['policy_kwargs'] = dict(
             input_width=84,
             input_height=84,
             input_channels=3,
@@ -166,7 +196,8 @@ if __name__ == "__main__":
             hidden_sizes=[256,256,256,256],
             init_w=1e-4
         )
-    )
-    setup_logger('sac-diagonal', variant=variant)
-    # ptu.set_gpu_mode(True)  # optionally set the GPU (default=False)
+    else:
+        variant['policy_kwargs'] = dict()
+    if args.gpu:
+        ptu.set_gpu_mode(True)  # optionally set the GPU (default=False)
     experiment(variant)
