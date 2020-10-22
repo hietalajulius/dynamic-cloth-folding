d11887d6d8361c0f8b3d29acb0826053648b7a91
Julius Hietala <julius@smartly.io>
2020-10-22 14:59:02 +0300

    Sac training update

--- experiments/sac_training.py
+++ experiments/sac_training.py
@@ -5,10 +5,10 @@ from rlkit.data_management.env_replay_buffer import EnvReplayBuffer
 from rlkit.envs.wrappers import NormalizedBoxEnv
 from rlkit.launchers.launcher_util import setup_logger
 from rlkit.samplers.data_collector import MdpPathCollector
-from rlkit.samplers.data_collector import GoalConditionedPathCollector
+from rlkit.samplers.data_collector import GoalConditionedPathCollector, KeyPathCollector
 from rlkit.torch.sac.policies import TanhGaussianPolicy, MakeDeterministic, TanhCNNGaussianPolicy, MonsterTanhCNNGaussianPolicy
 from rlkit.torch.sac.sac import SACTrainer
-from rlkit.torch.her.her import HERTrainer
+from rlkit.torch.her.her import ClothSacHERTrainer
 from rlkit.torch.networks import ConcatMlp, MergedCNN
 from rlkit.torch.torch_rl_algorithm import TorchBatchRLAlgorithm
 from rlkit.data_management.obs_dict_replay_buffer import ObsDictRelabelingBuffer
@@ -36,6 +36,8 @@ def argsparser():
     parser.add_argument('--n_actions', type=int, default=3)
     parser.add_argument('--learn_grasp', type=str, default=False)
     parser.add_argument('--distance_threshold', type=float, default=0.05)
+    parser.add_argument('--eval_steps', type=int, default=500)
+    parser.add_argument('--min_expl_steps', type=int, default=1000)
     return parser.parse_args()
 
 
@@ -50,6 +52,8 @@ def experiment(variant):
         path_collector_observation_key = 'observation'
 
     obs_dim = expl_env.observation_space.spaces['observation'].low.size
+    robot_obs_dim = expl_env.observation_space.spaces['robot_observation'].low.size
+    model_params_dim = expl_env.observation_space.spaces['model_params'].low.size
     action_dim = eval_env.action_space.low.size
     goal_dim = eval_env.observation_space.spaces['desired_goal'].low.size
 
@@ -61,53 +65,55 @@ def experiment(variant):
     M = variant['layer_size']
 
     qf1 = ConcatMlp(
-    input_size=obs_dim + action_dim + goal_dim,
+    input_size=obs_dim + action_dim + model_params_dim + goal_dim,
     output_size=1,
     hidden_sizes=[M, M],
     )
     qf2 = ConcatMlp(
-        input_size=obs_dim + action_dim + goal_dim,
+        input_size=obs_dim + action_dim + model_params_dim + goal_dim,
         output_size=1,
         hidden_sizes=[M, M],
     )
     target_qf1 = ConcatMlp(
-        input_size=obs_dim + action_dim + goal_dim,
+        input_size=obs_dim + action_dim + model_params_dim + goal_dim,
         output_size=1,
         hidden_sizes=[M, M],
     )
     target_qf2 = ConcatMlp(
-        input_size=obs_dim + action_dim + goal_dim,
+        input_size=obs_dim + action_dim + model_params_dim + goal_dim,
         output_size=1,
         hidden_sizes=[M, M],
     )
     if image_training:
         policy = TanhCNNGaussianPolicy(
             output_size=action_dim,
-            added_fc_input_size=goal_dim,
+            added_fc_input_size=robot_obs_dim + goal_dim,
             **variant['policy_kwargs'],
         )
     else:
         policy = TanhGaussianPolicy(
-            obs_dim=obs_dim + goal_dim,
+            obs_dim=obs_dim + model_params_dim + goal_dim,
             action_dim=action_dim,
             hidden_sizes=[M, M],
             **variant['policy_kwargs']
         )
 
     eval_policy = MakeDeterministic(policy)
-    eval_path_collector = GoalConditionedPathCollector(
+    eval_path_collector = KeyPathCollector(
         eval_env,
         eval_policy,
         render=True,
         render_kwargs={'mode': 'rgb_array'},
         observation_key=path_collector_observation_key,
-        desired_goal_key=desired_goal_key
+        desired_goal_key=desired_goal_key,
+        **variant['path_collector_kwargs']
     )
-    expl_path_collector = GoalConditionedPathCollector(
+    expl_path_collector = KeyPathCollector(
         expl_env,
         policy,
         observation_key=path_collector_observation_key,
-        desired_goal_key=desired_goal_key
+        desired_goal_key=desired_goal_key,
+        **variant['path_collector_kwargs']
     )
     replay_buffer = ObsDictRelabelingBuffer(
         env=eval_env,
@@ -125,7 +131,7 @@ def experiment(variant):
         target_qf2=target_qf2,
         **variant['trainer_kwargs']
     )
-    trainer = HERTrainer(trainer)
+    trainer = ClothSacHERTrainer(trainer)
     algorithm = TorchBatchRLAlgorithm(
         trainer=trainer,
         exploration_env=expl_env,
@@ -139,6 +145,8 @@ def experiment(variant):
     with mujoco_py.ignore_mujoco_warnings():
         algorithm.train()
 
+    return eval_policy
+
 
 
 
@@ -147,16 +155,6 @@ if __name__ == "__main__":
     variant = dict(
         algorithm="SAC",
         layer_size=256,
-        replay_buffer_kwargs=dict(
-            max_size=int(1E6),
-            fraction_goals_env_goals=0,
-        ),
-        algorithm_kwargs=dict(
-            num_epochs=100,
-            num_eval_steps_per_epoch=500,
-            min_num_steps_before_training=1000,
-            batch_size=256,
-        ),
         trainer_kwargs=dict(
             discount=0.99,
             soft_target_tau=5e-3,
@@ -166,21 +164,33 @@ if __name__ == "__main__":
             reward_scale=1,
             use_automatic_entropy_tuning=True,
         ),
+        path_collector_kwargs = dict(),
+        policy_kwargs = dict(),
+        replay_buffer_kwargs=dict(),
+        algorithm_kwargs=dict(),
     )
     args = argsparser()
-    file_path = args.title + "-run-" + str(args.run)
-
-    variant['algorithm_kwargs']['num_trains_per_train_loop'] = args.train_steps
-    variant['algorithm_kwargs']['num_expl_steps_per_train_loop'] = args.train_steps
-    variant['algorithm_kwargs']['num_train_loops_per_epoch'] = int(10000 / args.train_steps)
-    variant['algorithm_kwargs']['num_epochs'] = args.num_epochs
-    variant['algorithm_kwargs']['max_path_length'] = int(args.max_path_length)
-    variant['replay_buffer_kwargs']['fraction_goals_rollout_goals'] = 1 - args.her_percent
-    variant['replay_buffer_kwargs']['max_size'] = int(args.buffer_size)
     variant['env_name'] = args.env_name
     variant['version'] = args.title
     variant['image_training'] = args.image_training
     variant['asymmetric'] = args.asymmetric
+
+    variant['algorithm_kwargs'] = dict(
+        num_epochs=args.num_epochs,
+        num_trains_per_train_loop = args.train_steps,
+        num_expl_steps_per_train_loop = args.train_steps,
+        num_train_loops_per_epoch = int(10000 / args.train_steps),
+        max_path_length = int(args.max_path_length),
+        num_eval_steps_per_epoch=args.eval_steps,
+        min_num_steps_before_training=args.min_expl_steps,
+        batch_size=256,
+    )
+
+    variant['replay_buffer_kwargs'] =dict(
+            max_size=int(args.buffer_size),
+            fraction_goals_env_goals = 0,
+            fraction_goals_rollout_goals = 1 - args.her_percent
+        )
 
     variant['env_kwargs'] = dict(
         learn_grasp = args.learn_grasp,
@@ -191,7 +201,6 @@ if __name__ == "__main__":
         distance_threshold=args.distance_threshold
     )
 
-    setup_logger(file_path, variant=variant)
     if args.image_training:
         variant['policy_kwargs'] = dict(
             input_width=84,
@@ -204,23 +213,19 @@ if __name__ == "__main__":
             hidden_sizes=[256,256,256,256],
             init_w=1e-4
         )
-        if args.asymmetric:
-            variant['replay_buffer_kwargs']['internal_keys'] = ['image']
-        else:
-            variant['value_kwargs'] = dict(
-                input_width=84,
-                input_height=84,
-                input_channels=3,
-                kernel_sizes=[3,3,3,3],
-                n_channels=[32,32,32,32],
-                strides=[2,2,2,2],
-                paddings=[0,0,0,0],
-                hidden_sizes=[256,256,256,256],
-                init_w=1e-4
-        
-        )
+        variant['path_collector_kwargs']['additional_keys'] = ['robot_observation']
+        variant['replay_buffer_kwargs']['internal_keys'] = ['image','model_params','robot_observation']
+
+    else:
+        variant['path_collector_kwargs']['additional_keys'] = ['model_params']
+        variant['replay_buffer_kwargs']['internal_keys'] = ['model_params']
 
     if args.gpu:
         ptu.set_gpu_mode(True)  # optionally set the GPU (default=False)
         print("GPU training", ptu)
-    experiment(variant)
+
+    file_path = args.title + "-run-" + str(args.run)
+    setup_logger(file_path, variant=variant)
+
+    trained_policy = experiment(variant)
+    torch.save(policy.state_dict(), path +'.mdl')
