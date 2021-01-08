7e964e50f5cee8becc20c7a4b7acd9786e33b28e
Julius Hietala <julius@smartly.io>
2021-01-08 12:44:06 +0200

    rgb

--- experiments/rgb_test.py
+++ experiments/rgb_test.py
@@ -2,21 +2,58 @@ import numpy as np
 from rlkit.envs.wrappers import NormalizedBoxEnv
 from utils import argsparser, get_variant
 import gym
+from rlkit.samplers.data_collector import PresetEvalKeyPathCollector
+from rlkit.torch.sac.policies import TanhCNNGaussianPolicy, MakeDeterministic
+import torch
+
 
 def experiment(variant):
     eval_env = NormalizedBoxEnv(
         gym.make(variant['env_name'], **variant['env_kwargs']))
 
+    obs_dim = eval_env.observation_space.spaces['observation'].low.size
+    robot_obs_dim = eval_env.observation_space.spaces['robot_observation'].low.size
+    model_params_dim = eval_env.observation_space.spaces['model_params'].low.size
+    action_dim = eval_env.action_space.low.size
+    goal_dim = eval_env.observation_space.spaces['desired_goal'].low.size
 
-    
-    eval_env.reset()
-    
-    while True:
-        eval_env.render()
-        obs, r, d, info = eval_env.step(np.array([0,0,0]))
-        print("Step")
+    image_training = variant['image_training']
+    if image_training:
+        path_collector_observation_key = 'image'
+    else:
+        path_collector_observation_key = 'observation'
 
+    observation_key = 'observation'
+    desired_goal_key = 'desired_goal'
+    achieved_goal_key = desired_goal_key.replace("desired", "achieved")
 
+    policy = TanhCNNGaussianPolicy(
+        output_size=action_dim,
+        added_fc_input_size=robot_obs_dim + goal_dim,
+        aux_output_size=12,
+        **variant['policy_kwargs'],
+    )
+
+    device = torch.device('cpu')
+
+    policy.load_state_dict(torch.load(
+        'current_policy.mdl', map_location=device))
+
+    eval_policy = MakeDeterministic(policy)
+
+    preset_eval_path_collector = PresetEvalKeyPathCollector(
+        eval_env,
+        eval_policy,
+        render=True,
+        observation_key=path_collector_observation_key,
+        desired_goal_key=desired_goal_key,
+        **variant['path_collector_kwargs']
+    )
+
+    preset_eval_path_collector.collect_new_paths(
+        100,
+        10
+    )
 
 
 if __name__ == "__main__":
