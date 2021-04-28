5579440c655601e004b69e6dd50a68e4dda8f117
Julius Hietala <julius.hietala@gmail.com>
2021-04-28 17:52:55 +0300

    serialize

--- experiments/serialize_torch_module.py
+++ experiments/serialize_torch_module.py
@@ -13,7 +13,7 @@ def main(variant):
 
     goal_dim = eval_env.observation_space.spaces['desired_goal'].low.size
     action_dim = eval_env.action_space.low.size
-    added_fc_input_size = 0# goal_dim
+    added_fc_input_size =  goal_dim + 9
 
     '''
     policy = TanhCNNGaussianPolicy(
@@ -31,6 +31,10 @@ def main(variant):
                 aux_output_size=8,
                 **variant['policy_kwargs'],
             )
+
+    model_path = "/home/clothmanip/robotics/cloth-manipulation/experiments/trainings/[sidew_image_new-run-1_2021_04_22_18_57_28_0000--s-0] /policies/current_policy.mdl"
+    policy.load_state_dict(torch.load(model_path))
+    policy.eval()
 
     sm = torch.jit.script(policy).cpu()
     torch.jit.save(sm, "testjit.pt")
